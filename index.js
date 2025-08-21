8e5417d288aaf468ee8f5547122be25f8e366175
TaTia <tatyanakoryachkina@yandex.ru>
2025-08-21 18:02:35 +0300

    feat: create home-page

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-
 function saveName() {
     const userNameInput = document.getElementById('userNameInput');
     const userName = userNameInput.value;
     console.log('Сохраненное имя: ' + userName);
     localStorage.setItem('userName', userName);
 }
+
+let toggleButton = document.getElementById('toggleButton');
+
+
+// Добавляем обработчик события клика на кнопку
+toggleButton.addEventListener('click', () => {
+    const screen1 = document.getElementById('screen1');
+    const screen2 = document.getElementById('screen2');
+    screen1.classList.remove('visible');
+    screen1.classList.add('hidden');
+    screen2.classList.remove('hidden');
+    screen2.classList.add('visible');
+
+});
+
 
 
 
